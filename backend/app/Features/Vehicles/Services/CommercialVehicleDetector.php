89d826f76a8def64da7f81eb97c1fa604c2fbab6
rajoverseas9951-cpu <rajoverseas9951@gmail.com>
2026-08-11 11:28:47 +0530

    expand commercial vehicle detection aliases

--- backend/app/Features/Vehicles/Services/CommercialVehicleDetector.php
+++ backend/app/Features/Vehicles/Services/CommercialVehicleDetector.php
@@ -4,7 +4,7 @@ namespace App\Features\Vehicles\Services;
 
 final class CommercialVehicleDetector
 {
-    private const PATTERN = '/\b(?:COMMERCIAL|GOODS?|TRANSPORT|HGV|LGV|MGV|HMV|TRUCK|LORRY|TRAILER|PICK\s*UP|PICKUP|BUS|TAXI|CAB|MAXI|PSV|PASSENGER|STAGE\s+CARRIAGE|CONTRACT\s+CARRIAGE)\b/i';
+    private const PATTERN = '/\b(?:COMMERCIAL|GOODS?|TRANSPORT|HGV|HGVT|HGMV|HMV|LGV|LGVT|LCV|MGV|GT|TRUCK|LORRY|TIPPER|DUMPER|TRAILER|PICK\s*UP|PICKUP|BUS|OMNI\s*BUS|SCHOOL\s*BUS|AMBULANCE|TAXI|MOTOR\s*CAB|CAB|MAXI\s*CAB|MAXI|LPV|PSV|PASSENGER|STAGE\s*CARRIAGE|CONTRACT\s*CARRIAGE)\b/i';
 
     /** @param iterable<mixed> $values */
     public static function matches(iterable $values): bool
